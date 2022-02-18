b578fef5811f9183879606cab8176e24148c99b5
Tsvirko Nastya <92594745+Tsvirko-Nastya2007@users.noreply.github.com>
2022-02-18 19:32:02 +0300

    Space-shooter-1
    Space-shooter-1

---
+++ settings.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+
+const float WINDOW_HEIGHT = 900;
+const float WINDOW_WIDTH = 600;
+const size_t FPS = 60;
+const std::string WINDOW_TITLE = "Space-Shooter";
+const std::string IMAGES_FOLDER = "PNG/";
+const std::string PLAYER_FILE_NAME = "playerShip2_green.png";
